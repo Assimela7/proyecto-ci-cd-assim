cdcc21c3d50fe2bb579bec0a1eb1f2420ff3f3ad
Assimela7 <assimela7@gmail.com>
2026-03-16 16:37:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hola, servidor desplegado por Assim!"
+    return "Hola, servidor CI/CD actualizado con webhook!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
